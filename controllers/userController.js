4f7225aeeb0131b292494297c71ca98164b7eab0
cristian <cristiandecoud@hotmail.com>
2025-03-04 14:52:31 -0300

    Set access_token cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,12 +16,15 @@ export const register = async (req, res) => {
     }
 
     let token = jwt.sign({ id: user.id }, process.env.secretKey, {
-      expiresIn: 1 * 24 * 60 * 60, // 24 hours
+      expiresIn: '1h'
     });
 
-    res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
-
-    return res.json(201).send(user);
+    res.cookie("access_token", token, {
+      maxAge: 1 * 24 * 60 * 60, 
+      httpOnly: false,
+      // secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none'
+    }).json(201).send({user, token});
   } catch (error) {
     console.log(error);
   }
@@ -30,36 +33,33 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(email, password);
-    //find a user by their email
+    
     const user = await User.findOne({
-      where: {
-        email,
-      },
+      where: { email },
     });
 
-    console.log(user);
-    //if user email is found, compare password with bcrypt
     if (user) {
       const isSame = await bcrypt.compare(password, user.password);
-      //if password is the same
-      //generate token with the user's id and the secretKey in the env file
       if (isSame) {
         let token = jwt.sign(
-          { id: user.dataValues.id },
+          { id: user.id , username: user.username, email: user.email },
           process.env.secretKey,
-          {
-            expiresIn: 1 * 24 * 60 * 60 * 1000,
-          }
+          { expiresIn: '1h' }
         );
 
-        //if password matches wit the one in the database
-        //go ahead and generate a cookie for the user
-        res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
-        console.log("user", JSON.stringify(user, null, 2));
+        res.cookie("access_token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: 'Lax',
+        });
+
         console.log(token);
-        //send user data
-        return res.status(201).send(user);
+
+        return res.status(201).send({
+          id: user.id,
+          username: user.username,
+          email: user.email
+        });
       } else {
         return res.status(401).send("Authentication failed 1");
       }
--- server.js
+++ server.js
@@ -4,21 +4,28 @@ import cookieParser from 'cookie-parser';
 import db from './models/index.js';
 import userRoutes from './routes/userRoutes.js';
 import { PORT } from './config.js';
-import 'dotenv/config'
+import 'dotenv/config';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: 'http://localhost:4321', // Solo permite este dominio
+  credentials: true,
+  methods: ['GET', 'POST'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos
+};
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 db.sequelize.sync().then(() => {
 	console.log('\x1b[36m', "Database has been re sync", '\x1b[0m');
 })
 
-
 app.use("/api/users", userRoutes)
-
 
 app.listen(PORT, () => {
   console.log('\x1b[36m', `Server is running on port ${PORT}` ,'\x1b[0m');
